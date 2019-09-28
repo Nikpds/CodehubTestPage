ec6bcf4c4e3bc4ebee4ab74c3559d26deb5a5bac
nperperidis <perpegr@hotmail.com>
2019-09-28 17:35:44 +0300

    Updates

---
+++ precache-manifest.ab77c56d63761864e285008914948fb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cd72b59e200c33256d9a05a4d4795fb",
+    "url": "/CodehubTestPage/index.html"
+  },
+  {
+    "revision": "f979cae1b1104fe62bef",
+    "url": "/CodehubTestPage/static/css/2.1ab5750d.chunk.css"
+  },
+  {
+    "revision": "e9400871a8cfc097d8ce",
+    "url": "/CodehubTestPage/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "f979cae1b1104fe62bef",
+    "url": "/CodehubTestPage/static/js/2.1a0e930b.chunk.js"
+  },
+  {
+    "revision": "e9400871a8cfc097d8ce",
+    "url": "/CodehubTestPage/static/js/main.d67fca6f.chunk.js"
+  },
+  {
+    "revision": "b2c06499c9c54b9d3ebb",
+    "url": "/CodehubTestPage/static/js/runtime-main.2c8dd776.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/CodehubTestPage/static/media/logo.25bf045c.svg"
+  }
+]);
